67a0cf97ae9b3dbbd4c9eae06f88b2028b84d32c
sanjaypj <sanjaypjayan@gmail.com>
2019-12-07 11:44:00 -0800

    database added for faster data interaction

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const ipc = require('electron').ipcMain
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -36,7 +38,7 @@ function createWindow () {
 function createDetailsWindow (arg) {
   detailsWindow = new BrowserWindow({
     width: 600,
-    height: 600,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
@@ -80,27 +82,15 @@ function createSettingsWindow () {
   settingsWindow.setMenuBarVisibility(false)
 }
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 
 ipc.on('open:details', function (event, arg) {
@@ -108,8 +98,15 @@ ipc.on('open:details', function (event, arg) {
 })
 
 ipc.on('update:subreddit', function (event, arg) {
-  subreddit = arg;
-  mainWindow.subreddit = subreddit;
+  db.serialize(function() {
+    db.run("DROP TABLE IF EXISTS subreddit");
+
+    db.run("CREATE TABLE IF NOT EXISTS subreddit (info TEXT)");
+  
+    db.run("INSERT INTO subreddit (info) VALUES ('" + arg + "')");
+  });
+  
+  // mainWindow.subreddit = subreddit;
   mainWindow.reload()
 })
 
